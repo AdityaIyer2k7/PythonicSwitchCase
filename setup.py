520b3d46c1209b15c758d68d75deef1a282818fe
DrSparky-2007 <56183042+DrSparky-2007@users.noreply.github.com>
2020-12-07 10:42:15 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,9 @@
+import setuptools, os, pathlib
+
+BASE_DIR = pathlib.Path(__file__).parent
+README = BASE_DIR / 'README.md'
+LICENSE = BASE_DIR / 'LICENSE'
+
+setuptools.setup(
+    
+)
---
+++ switch/__init__.py
@@ -0,0 +1,49 @@
+class Switch:
+    def __init__(self, var = None, errOnCaseExist = True, fast = False):
+        import threading
+        self.var = var
+        self.errOnCaseExist = errOnCaseExist
+        self.valFunc = {None: lambda: print("Default function")}
+        self.funcToRun = self.valFunc[None]
+        self.fast = fast
+        if fast:
+            self.checking = True
+            checkChangeThread = threading.Thread(target=self.checkChange)
+            checkChangeThread.start()
+
+    def changeFuncToRun (self):
+        var = self.var
+        if var in self.valFunc.keys():
+            self.funcToRun = self.valFunc[var]
+        else:
+            self.funcToRun = self.valFunc[None]
+
+    def checkChange (self):
+        lastVar = self.var
+        while self.checking:
+            var = self.var
+            if var != lastVar:
+                self.changeFuncToRun()
+                lastVar = var
+    
+    def case (self, value, func, *args, **kwargs):
+        if value in self.valFunc.keys() and self.errOnCaseExist:
+            raise KeyError("Value already exists")
+        else:
+            self.valFunc[value] = lambda: func(*args, **kwargs)
+        self.changeFuncToRun()
+    
+    def default (self, func, *args, **kwargs):
+        self.valFunc[None] = lambda: func(*args, **kwargs)
+
+    def __call__(self):
+        if self.fast:
+            self.funcToRun()
+        else:
+            if var in self.valFunc.keys():
+                self.valFunc[var]()
+            else:
+                self.valFunc[None]()
+
+    def kill(self):
+        self.checking = False
